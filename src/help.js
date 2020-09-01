e93002ea5b71e02c924b848e8525715c45602e94
Rithik Yelisetty <rithik@gmail.com>
2020-09-01 15:03:49 -0400

    Add separate table for classes in help

--- src/help.js
+++ src/help.js
@@ -1,7 +1,10 @@
 // @flow 
 
 import type {CommandNames, CommandDataTableType, ColumnDataTableType} from './commands.js';
+import type {ClassCommands, JoinOrDiscussType, ClassType} from './classes.js';
+
 import COMMANDS from './commands.js';
+import CLASSES from './classes.js';
 
 const viewHelpPage: () => void = function(){
     const data: Array<CommandDataTableType> = Object.keys(COMMANDS).map((command: CommandNames) => {
@@ -21,6 +24,29 @@ const viewHelpPage: () => void = function(){
     $('#help-table').DataTable({
         data: data,
         columns: columns,
+        order: [[ 1, "asc" ]],
+        paging: false
+    });
+
+    const classesData: Array<CommandDataTableType> = Object.keys(CLASSES).map((command: ClassCommands) => {
+        const cmdData = CLASSES[command];
+        return {
+            name: cmdData.name, 
+            url: cmdData.url, 
+            // $FlowFixMe - this is actually correct.
+            command: command
+        };
+    });
+
+    const classColumns: Array<ColumnDataTableType> = [
+        {data: 'command', title: "Command"}, 
+        {data: 'name', title: "Name"}, 
+        {data: 'url', title: "URL"}, 
+    ];
+    // $FlowFixMe - jQuery import
+    $('#classes-table').DataTable({
+        data: classesData,
+        columns: classColumns,
         order: [[ 1, "asc" ]],
         paging: false
     });
